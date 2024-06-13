6c1d989367ab23f555f15910b2540786a417475d
omosteven <omosteven123@gmail.com>
2024-06-13 14:15:04 +0100

    simplified routes

--- src/app/routes/index.ts
+++ src/app/routes/index.ts
@@ -9,9 +9,9 @@ const router = Router();
 
 const rootRouter = () => {
   router.use(usersRoute());
-  router.use(postsRoute);
-  router.use(commentsRoute);
-  router.use(mediaRoute);
+  router.use(postsRoute());
+  router.use(commentsRoute());
+  router.use(mediaRoute());
 
   return router;
 };
